a2fd55a61c5895d5178ed69816f66188fe3cbc2c
gumsanmarip <gumsanmarip@gmail.com>
2022-02-11 02:18:04 -0500

    working

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,16 +1,65 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+
+const initialState = {
+  x: '2',
+  y: '2',
+  steps: 0,
+  email: '',
+}
+const URL = 'http://localhost:9000/api/result';
 
 export default class AppClass extends React.Component {
-  //steps
-  constructor(props){
-    super(props);
-    this.state = {
-      step: 0
-    };
+  state = initialState;
+
+  activeSquare = { x: 2, y: 2}
+  
+
+  componentDidMount(){
+    this.state.x.y;
   }
 
+  //coordinates 
+  moveCoord = () => {
+    this.set
+  
+    up.y--;
+    down.y++;
+    left.x--;
+    right.x++;
+ 
+  }
 
+  //submit, email error, post URL
+  onSubmit = e => {
+    e.preventDefault();
+    const payloadToSend = { email: this.state.email}
+    axios.post(URL, payloadToSend)
+      .then(res => {
+        this.setState({ 
+          ...this.state, 
+          message: res.data.message })
+      })
+      .catch(err => {
+        const errorFromAPI = err.response.data.message
+        this.setState({ ...this.state, error: errorFromAPI })
+      })
 
+  }
+
+  onError = err => {
+    const errorFromAPI = err.response.data.message
+    this.setState({ ...this.state, error: errorFromAPI })
+  }
+
+  //reset x, y, steps-
+  handleReset = () => {
+    this.setState({ ...initialState});
+  }
+  //steps-
+  stepCounter = () => { 
+    this.setState({ steps: this.state.steps + 1 })
+  }
 
   render() {
     const { className } = this.props
@@ -18,7 +67,7 @@ export default class AppClass extends React.Component {
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved {this.state.step} times</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -32,18 +81,18 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.error}</h3>
         </div>
         <div id="keypad">
-          <button id="left" onClick={() => this.setState({ step: this.state.step + 1 })}>LEFT</button>
-          <button id="up" onClick={() => this.setState({ step: this.state.step + 1 })}>UP</button>
-          <button id="right" onClick={() => this.setState({ step: this.state.step + 1 })}>RIGHT</button>
-          <button id="down" onClick={() => this.setState({ step: this.state.step + 1 })}>DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" value={this.state.x} onClick={this.stepCounter}>LEFT</button>
+          <button id="up" value={this.state.y} onClick={this.stepCounter}>UP</button>
+          <button id="right" value={this.state.x} onClick={this.stepCounter}>RIGHT</button>
+          <button id="down" value={this.state.y} onClick={this.stepCounter}>DOWN</button>
+          <button id="reset" onClick={this.handleReset}>reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input id="email" type="email" placeholder="type email" onSubmit={this.props.onSubmit} value={this.props.email}></input>
+          <input id="submit" type="submit" ></input>
         </form>
       </div>
     )
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,19 +1,43 @@
 import React, {useState} from 'react'
 
+const initialState = {
+  x: '2',
+  y: '2',
+  steps: 0,
+  email: '',
+};
+const URL = 'http://localhost:9000/api/result';
+
 export default function AppFunctional(props) {
+  // const [{x, y, steps, email}, setState] = useState(initialState);
+
+  const handleChanges = (e) => {
+    set
+  }
+  //position
+  const posArray = [
+    [0,0,0],
+    [0,1,0],
+    [0,0,0],
+  ]
+  posArray[1][1];
 
   //coordinates
-  // const [coord, setCoord] = useState(x,y);
-  //step movement
-  const [step, setStep] = useState(0);
+  
+  const [steps, setStep] = useState(0);
 
+  //reset x,y,steps
+  // const handleReset = () => {
+  //   setState({ ...initialState})
+
+  // }
 
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved {step} times</h3>
+        <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         <div className="square"></div>
@@ -30,11 +54,11 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={() => setStep(step + 1)}>LEFT</button>
-        <button id="up" onClick={() => setStep(step + 1)}>UP</button>
-        <button id="right" onClick={() => setStep(step + 1)}>RIGHT</button>
-        <button id="down" onClick={() => setStep(step + 1)}>DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => useStep(steps + 1)}>LEFT</button>
+        <button id="up" onClick={() => setStep(steps + 1)}>UP</button>
+        <button id="right" onClick={() => setStep(steps + 1)}>RIGHT</button>
+        <button id="down" onClick={() => setStep(steps + 1)}>DOWN</button>
+        <button id="reset" >reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
