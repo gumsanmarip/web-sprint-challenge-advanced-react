8addf1eecb6f3919bb19d152a4836d96c4eb9475
gumsanmarip <gumsanmarip@gmail.com>
2022-02-10 19:43:55 -0500

    start

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 export default class AppClass extends React.Component {
+  //steps
+  constructor(props){
+    super(props);
+    this.state = {
+      step: 0
+    };
+  }
+
+
+
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="steps">You moved {this.state.step} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -24,10 +35,10 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button id="left" onClick={() => this.setState({ step: this.state.step + 1 })}>LEFT</button>
+          <button id="up" onClick={() => this.setState({ step: this.state.step + 1 })}>UP</button>
+          <button id="right" onClick={() => this.setState({ step: this.state.step + 1 })}>RIGHT</button>
+          <button id="down" onClick={() => this.setState({ step: this.state.step + 1 })}>DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AppFunctional(props) {
+
+  //coordinates
+  // const [coord, setCoord] = useState(x,y);
+  //step movement
+  const [step, setStep] = useState(0);
+
+
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="steps">You moved {step} times</h3>
       </div>
       <div id="grid">
         <div className="square"></div>
@@ -22,10 +30,10 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={() => setStep(step + 1)}>LEFT</button>
+        <button id="up" onClick={() => setStep(step + 1)}>UP</button>
+        <button id="right" onClick={() => setStep(step + 1)}>RIGHT</button>
+        <button id="down" onClick={() => setStep(step + 1)}>DOWN</button>
         <button id="reset">reset</button>
       </div>
       <form>
