a7203e1559829fac12ba17f83ea3cef912155d92
gumsanmarip <gumsanmarip@gmail.com>
2022-03-02 20:06:40 -0500

    appClass fixed

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -2,56 +2,71 @@ import React from 'react';
 import axios from 'axios';
 
 const initialState = {
-  x: '2',
-  y: '2',
+  coord: {
+    x: 2,
+    y: 2
+  },
   steps: 0,
   email: '',
+  message: '',
 }
-const posArray = [
-  [0,0,0],
-  [0,1,0],
-  [0,0,0],
-];
-console.log(posArray[1],[1])
-const boxes = [];
-
-for (let i = 0; i < posArray.length; i++){
-  boxes[i] = [];
-  for (let j= 0; j < posArray.length; j++){
-    boxes[i][j] = null;
-  }
-}
-//console.log(boxes);
 
 export default class AppClass extends React.Component {
   state = initialState;
-  
-  //coordinates
-  updateCoord = () => {
-    this.setState({
-      ...this.state, 
-      
-    })
-    
-  }
 
+  //movements
   moveLeft = () => {
-    this.setState({ x: this.state.x -1})
+    if (this.state.coord.x > 1){
+      this.setState({ ...this.state, 
+        coord: {... this.state.coord, x: this.state.coord.x - 1},
+        message: '',
+        steps: this.state.steps + 1
+      })
+    }
+    else {
+      this.setState({ ...this.state, message: "You can't go left"})
+    }
   }
   moveRight = () => {
-    this.setState({ x: this.state.x +1})
+    if (this.state.coord.x < 3){
+      this.setState({ ...this.state, 
+        coord: {... this.state.coord, x: this.state.coord.x + 1},
+        message: '',
+        steps: this.state.steps + 1
+      })
+    }
+    else {
+      this.setState({ ...this.state, message: "You can't go right"})
+    }
   }
   moveUp = () => {
-    this.setState({ y: this.state.y +1})
+    if (this.state.coord.y > 1){
+      this.setState({ ...this.state, 
+        coord: {... this.state.coord, y: this.state.coord.y - 1},
+        message: '',
+        steps: this.state.steps + 1
+      })
+    }
+    else {
+      this.setState({ ...this.state, message: "You can't go "})
+    }
   }
   moveDown = () => {
-    this.setState({ y: this.state.y -1})
+    if (this.state.coord.y < 3){
+      this.setState({ ...this.state, 
+        coord: {... this.state.coord, y: this.state.coord.y + 1},
+        message: '',
+        steps: this.state.steps + 1
+      })
+    }
+    else {
+      this.setState({ ...this.state, message: "You can't go down"})
+    }
   }
-
-  //steps-
-  stepCounter = () => { 
-    this.setState({ steps: this.state.steps + 1 })
-  }
+  
+  // stepCounter = () => { 
+  //   this.setState({ steps: this.state.steps + 1 })
+  // }
 
   //reset x, y, steps-
   handleReset = () => {
@@ -66,16 +81,15 @@ export default class AppClass extends React.Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    const payload = {x: this.state.x, y: this.state.y, steps: this.state.steps, email: this.state.email};
+    const payload = {x: this.state.coord.x, y: this.state.coord.y, steps: this.state.steps, email: this.state.email};
     axios.post('http://localhost:9000/api/result', payload)
       .then(res => {
         this.setState({ ...this.state, message: res.data.message})
+        this.setState({ ...this.state, email: ''})
       })
       .catch(err => {
-        const errorFromAPI = err.response.data.message
-        this.setState({ ...this.state, message: errorFromAPI });
-      })
-    this.handleReset();  
+        this.setState({ ...this.state, message: err.response.data.message });
+      })  
   }
 
   render() {
@@ -83,28 +97,28 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({this.state.x},{this.state.y}) </h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.coord.x},${this.state.coord.y})`}) </h3>
           <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
-          <div className="square" id='1'></div>
-          <div className="square" id='2'></div>
-          <div className="square" id='3'></div>
-          <div className="square" id='4'></div>
-          <div className="square active" id='5'>B</div>
-          <div className="square" id='6'></div>
-          <div className="square" id='7'></div>
-          <div className="square" id='8'></div>
-          <div className="square" id='9'></div>
+          <div className={`${this.state.coord.x == 1 && this.state.coord.y == 1 ? "square active" : "square"}`} id='1'>{this.state.coord.x === 1 && this.state.coord.y === 1 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 2 && this.state.coord.y == 1 ? "square active" : "square"}`} id='2'>{this.state.coord.x === 2 && this.state.coord.y === 1 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 3 && this.state.coord.y == 1 ? "square active" : "square"}`} id='3'>{this.state.coord.x === 3 && this.state.coord.y === 1 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 1 && this.state.coord.y == 2 ? "square active" : "square"}`} id='4'>{this.state.coord.x === 1 && this.state.coord.y === 1 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 2 && this.state.coord.y == 2 ? "square active" : "square"}`} id='5'>{this.state.coord.x === 2 && this.state.coord.y === 2 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 3 && this.state.coord.y == 2 ? "square active" : "square"}`} id='6'>{this.state.coord.x === 3 && this.state.coord.y === 2 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 1 && this.state.coord.y == 3 ? "square active" : "square"}`} id='7'>{this.state.coord.x === 1 && this.state.coord.y === 3 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 2 && this.state.coord.y == 3 ? "square active" : "square"}`} id='8'>{this.state.coord.x === 2 && this.state.coord.y === 3 ? "B" : ""}</div>
+          <div className={`${this.state.coord.x == 3 && this.state.coord.y == 3 ? "square active" : "square"}`} id='9'>{this.state.coord.x === 3 && this.state.coord.y === 3 ? "B" : ""}</div>
         </div>
         <div className="info">
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left" value={this.state.x} onClick={this.stepCounter}>LEFT</button>
-          <button id="up" value={this.state.y} onClick={this.stepCounter}>UP</button>
-          <button id="right" value={this.state.x} onClick={this.stepCounter}>RIGHT</button>
-          <button id="down" value={this.state.y} onClick={this.stepCounter}>DOWN</button>
+          <button id="left"  onClick={this.moveLeft}>LEFT</button>
+          <button id="up"    onClick={this.moveUp}>UP</button>
+          <button id="right" onClick={this.moveRight}>RIGHT</button>
+          <button id="down"  onClick={this.moveDown}>DOWN</button>
           <button id="reset" onClick={this.handleReset}>reset</button>
         </div>
         <form onSubmit={this.onSubmit}>
